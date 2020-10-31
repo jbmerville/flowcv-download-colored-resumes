a94f39d22387621ea54f7b2939ba9860ef6ca407
jbmerville <jbmer@my.yorku.ca>
2020-10-31 16:12:46 +0100

    Fix resume renaming bug

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ dirToDownloadResumesTo = "path/To/Dir"
 # Allowed colors:
 # (please forgive my color naming abilities... I tried :D):
 colors = ["black", "red", "wine", "grape", "purple", "ocean", "blue", "sky", "cyan", "swamp",
-          "green", "grass", "vomit", "yellow", "sun", "orange", "red", "brown", "gray", "metal"]
+          "green", "grass", "vomit", "yellow", "sun", "orange", "brown", "gray", "metal"]
 
 # Change this if you get errors similar to this:
 # Message: no such element: Unable to locate element: {...}
--- download-colored-resumes.py
+++ download-colored-resumes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from os import listdir, mkdir, rename
+from os import listdir, mkdir
 from re import search
+from shutil import move
 from time import sleep
 
 from selenium import webdriver
@@ -10,21 +11,29 @@ from config import colors, dirToDownloadResumesTo, email, password, sleepTime
 
 
 class FlowCVAction():
+    allowedColors = ["black", "red", "wine", "grape", "purple", "ocean", "blue", "sky", "cyan", "swamp",
+                     "green", "grass", "vomit", "yellow", "sun", "orange", "brown", "gray", "metal"]
+
     def __init__(self):
-        self.currentDate = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
-        self.downloadPath = "%s/%s" % (dirToDownloadResumesTo,
-                                       self.currentDate)
-        chrome_options = webdriver.ChromeOptions()
-        prefs = {
-            'download.default_directory': self.downloadPath}
-        chrome_options.add_experimental_option('prefs', prefs)
-        self.driver = webdriver.Chrome(
-            ChromeDriverManager().install(), options=chrome_options)
         self.isLoggedIn = False
         self.isOnDesignTab = False
+        self.areColorsValid()
+        self.currentDate = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+        self.downloadPath = "%s/%s" % (dirToDownloadResumesTo, self.currentDate)
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option('prefs', {'download.default_directory': self.downloadPath})
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+    def areColorsValid(self):
+        for color in colors:
+            if color not in self.allowedColors:
+                raise Exception("❌ Invalid color \"%s\", please use only the following colors in your \"config.py\": %s" % (color, self.allowedColors))
+        return True
+
+    def createDownloadDir(self):
         mkdir(self.downloadPath)
-        print("✅ Successfully created \"%s\" folder in \"%s\"" %
-              (self.currentDate, dirToDownloadResumesTo))
+        print("✅ Successfully created \"%s\" folder in \"%s\"" % (self.currentDate, dirToDownloadResumesTo))
 
     def executeSteps(self, name, steps):
         try:
@@ -38,36 +47,35 @@ class FlowCVAction():
                     sleep(step["sleepTime"])
             print("✅ Successfully executed \"%s\"" % name)
         except Exception as exception:
-            raise Exception("❌ %s failed with error:\n%s" %
-                            (name.capitalize(), exception))
+            raise Exception("❌ %s failed with error:\n%s" % (name.capitalize(), exception))
 
     def login(self):
         steps = [
             {
                 "name": "email_input",
-                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div/div/div[1]/input",
+                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div/div[1]/input",
                 "action": "send_keys",
                 "action_payload": email,
                 "sleepTime": 1
             },
             {
                 "name": "next_button",
-                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div/div/div[2]/button",
+                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div/div[2]/button",
                 "action": "click",
-                "sleepTime": sleepTime or 2
+                "sleepTime": sleepTime or 3
             },
             {
                 "name": "password_input",
-                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div/div[2]/div[1]/div/input",
+                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div[2]/div[1]/div/input",
                 "action": "send_keys",
                 "action_payload": password,
                 "sleepTime": 1
             },
             {
                 "name": "loggin_button",
-                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div/div[2]/div[2]/button",
+                "xPath": "/html/body/div/div[2]/div/div[4]/div/div/div/div[2]/div[2]/button",
                 "action": "click",
-                "sleepTime": sleepTime or 2
+                "sleepTime": sleepTime or 3
             },
         ]
         self.driver.get("https://flowcv.io/login")
@@ -94,30 +102,27 @@ class FlowCVAction():
         if not self.isOnDesignTab:
             raise Exception(
                 "❌ User not on Design tab. Please execute \"navigateToDesignTab\" before executing \"downloadColoredResumes\"")
+
+        self.createDownloadDir()
+
         colorsXPath = {}
-        colorsOrder = ["black", "red", "wine", "grape", "purple", "ocean", "blue", "sky", "cyan", "swamp",
-                       "green", "grass", "vomit", "yellow", "sun", "orange", "red", "brown", "gray", "metal"]
-        for index, color in enumerate(colorsOrder):
-            colorsXPath[color] = "/html/body/div[1]/div[2]/div/div[2]/div/div[1]/div[3]/div[11]/div/div[2]/div[2]/div/label[%d]" % (
-                index + 1)
+        for index, color in enumerate(self.allowedColors):
+            colorsXPath[color] = "/html/body/div[1]/div[2]/div/div[2]/div/div[1]/div[3]/div[11]/div/div[2]/div[2]/div/label[%d]" % (index + 1)
 
         for color in colors:
-            if color not in colorsXPath:
-                raise Exception("❌ Invalid color \"%s\", please use only the following colors in your \"config.py\": %s" % (
-                    color, colorsOrder))
             xPath = colorsXPath[color]
             steps = [
                 {
                     "name": "choose_resume_color_%s" % color,
                     "xPath": xPath,
                     "action": "click",
-                    "sleepTime": sleepTime or 2
+                    "sleepTime": 1
                 },
                 {
                     "name": "download_button",
                     "xPath": "/html/body/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/div/div[2]/button",
                     "action": "click",
-                    "sleepTime": sleepTime or 5
+                    "sleepTime": sleepTime or 10
                 },
             ]
 
@@ -129,23 +134,25 @@ class FlowCVAction():
     def moveResumeInsideFolder(self, color, renameFileWithoutDate=True):
         fileNames = listdir(self.downloadPath)
         pdfFileCount = 0
-        pdfFile = None
+        pdfFileName = None
+
         for fileName in fileNames:
             if ".pdf" in fileName:
                 pdfFileCount += 1
-                pdfFile = fileName
-        if pdfFile == None or pdfFileCount != 1:
+                pdfFileName = fileName
+
+        if pdfFileName == None or pdfFileCount != 1:
             raise Exception(
                 "❌ Excpected 1 \".pdf\" file in directory \"%s\" but found %d. Please execute moveResumeInsideFolder with maximum one \".pdf\" file in directory." % (self.downloadPath, pdfFileCount))
+
+        oldPathToPdfFile = "%s/%s" % (self.downloadPath, pdfFileName)
+        newPathToColorFolder = "%s/%s" % (self.downloadPath, color.capitalize())
+        mkdir(newPathToColorFolder)
+        newPathToPdfFile = "%s/%s" % (newPathToColorFolder, pdfFileName.replace(' ', '_'))
         if renameFileWithoutDate:
-            firstNumber = search("\d", pdfFile).start()
-            oldPathToPdfFile = "%s/%s" % (self.downloadPath, pdfFile)
-            newPathToColorFolder = "%s/%s" % (self.downloadPath,
-                                              color.capitalize())
-            mkdir(newPathToColorFolder)
-            newPathToPdfFile = "%s/%s.pdf" % (newPathToColorFolder,
-                                              pdfFile[:firstNumber - 1])
-            rename(oldPathToPdfFile, newPathToPdfFile)
+            firstNumber = search("\d", pdfFileName).start()
+            newPathToPdfFile = "%s/%s.pdf" % (newPathToColorFolder, pdfFileName[:firstNumber - 1].replace(' ', '_'))
+        move(oldPathToPdfFile, newPathToPdfFile)
 
 
 if __name__ == "__main__":
